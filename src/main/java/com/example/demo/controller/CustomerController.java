44871b272094d1f3b6e6c08d3bd6ee49a4d36ca8
Rajmohan P <rajmohan.p@tavant.com>
2018-10-13 10:34:00 -0500

    commit  Please enter the commit message for your changes. Lines starting

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,15 +1,14 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
+import com.example.demo.dao.CustomerDao;
+import com.example.demo.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.dao.CustomerDao;
-import com.example.demo.model.Customer;
+import java.util.List;
 
 @Controller
 public class CustomerController {
--- src/main/java/com/example/demo/dao/CustomerDao.java
+++ src/main/java/com/example/demo/dao/CustomerDao.java
@@ -1,10 +1,9 @@
 package com.example.demo.dao;
 
-import java.util.List;
-
+import com.example.demo.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.demo.model.Customer;
+import java.util.List;
 
 public interface CustomerDao extends CrudRepository<Customer, Long> {
 
